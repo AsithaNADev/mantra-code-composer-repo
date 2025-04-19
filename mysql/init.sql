875e642a0f36367bc325ac8abe2111efa5ae0b54
Madhawa Manamperi <madhawamanamperi@gmail.com>
2025-04-20 04:46:46 +0530

    update

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,18 +2,15 @@
 CREATE DATABASE IF NOT EXISTS `user`;
 CREATE DATABASE IF NOT EXISTS `customer`;
 CREATE DATABASE IF NOT EXISTS `loan`;
+CREATE DATABASE IF NOT EXISTS `transaction`;
 
 -- Create users and grant privileges (if they don't exist)
 CREATE USER IF NOT EXISTS 'appdev'@'%' IDENTIFIED BY 'appdev';
 GRANT ALL PRIVILEGES ON *.* TO 'appdev'@'%';
 
-CREATE USER IF NOT EXISTS 'appdev_user'@'%' IDENTIFIED BY 'appdev';
-GRANT ALL PRIVILEGES ON `user`.* TO 'appdev_user'@'%';
-
-CREATE USER IF NOT EXISTS 'appdev_customer'@'%' IDENTIFIED BY 'appdev';
-GRANT ALL PRIVILEGES ON `customer`.* TO 'appdev_customer'@'%';
-
-CREATE USER IF NOT EXISTS 'appdev_loan'@'%' IDENTIFIED BY 'appdev';
-GRANT ALL PRIVILEGES ON `loan`.* TO 'appdev_loan'@'%';
+GRANT ALL PRIVILEGES ON `transaction`.* TO 'appdev'@'%';
+GRANT ALL PRIVILEGES ON `user`.* TO 'appdev'@'%';
+GRANT ALL PRIVILEGES ON `customer`.* TO 'appdev'@'%';
+GRANT ALL PRIVILEGES ON `loan`.* TO 'appdev'@'%';
 
 FLUSH PRIVILEGES;
